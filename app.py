5ad007d313077a705aff95247c0def35410f92cf
Starias22 <adedeezechiel@gmail.com>
2024-08-22 01:00:51 +0100

    Application code updated

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import streamlit as st
 def main():
     st.title("Hello, Streamlit!")
     st.write("Build strategy: Source to Image (S2I)")
+    st.write("Application updated")
+
 
 if __name__ == "__main__":
     main()
